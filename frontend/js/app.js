f2029c190e6e8a555ed96596e29f5e1e354fcfbb
Aleksandr <101710507+meridian285@users.noreply.github.com>
2024-10-05 00:34:15 +0300

    added dashboard.html, dashboard.css

---
+++ frontend/js/app.js
@@ -0,0 +1,15 @@
+
+
+// const DATA_COUNT = 5;
+// const NUMBER_CFG = {count: DATA_COUNT, min: 0, max: 100};
+//
+// const data = {
+//     labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
+//     datasets: [
+//         {
+//             label: 'Dataset 1',
+//             data: Utils.numbers(NUMBER_CFG),
+//             backgroundColor: Object.values(Utils.CHART_COLORS),
+//         }
+//     ]
+// };
